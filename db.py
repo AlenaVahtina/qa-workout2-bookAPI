95f6c857d017511e8d23811bb680133e147619b3
avakhtina <https://mail.phoenixit.ru>
2021-04-02 01:06:31 +0300

    start db in memory

--- db.py
+++ db.py
@@ -1,4 +1,23 @@
 import sqlite3
+from sqlite3 import Error
 
-conn = sqlite3.connect("mydatabase.db")
-cursor = conn.cursor()
+
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect(':memory:')
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+def create_db(conn):
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE author(id int, FirstName varchar(32), LastName varchar(32), PRIMARY KEY (Id))")
+    cur.execute("CREATE TABLE book(id int, name varchar(100), year date, description text, PRIMARY KEY (Id))")
+    cur.execute("CREATE TABLE page(id int, book_id int, content text, is_torn boolean, PRIMARY KEY (Id))")
+    cur.execute("CREATE TABLE book_author(book_id int, author_id int)")
+
