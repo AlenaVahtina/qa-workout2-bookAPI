34fc45557e7735100fba80e6a7c0e2802d07a600
avakhtina <https://mail.phoenixit.ru>
2021-04-04 18:36:19 +0300

    db in class

--- db.py
+++ db.py
@@ -2,22 +2,35 @@ import sqlite3
 from sqlite3 import Error
 
 
-def create_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect(':memory:')
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
+class DB:
+    def __init__(self):
+        self.conn = None
 
+    def create_connection(self):
+        # conn = None
+        try:
+            self.conn = sqlite3.connect(':memory:')
+            print(sqlite3.version)
+        except Error as e:
+            print(e)
+        # finally:
+        #     if self.conn:
+        #         self.conn.close()
 
-def create_db(conn):
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE author(id int, FirstName varchar(32), LastName varchar(32), PRIMARY KEY (Id))")
-    cur.execute("CREATE TABLE book(id int, name varchar(100), year date, description text, PRIMARY KEY (Id))")
-    cur.execute("CREATE TABLE page(id int, book_id int, content text, is_torn boolean, PRIMARY KEY (Id))")
-    cur.execute("CREATE TABLE book_author(book_id int, author_id int)")
+    def create_db(self):
+        cur = self.conn.cursor()
+        cur.execute("CREATE TABLE author(id int, first_name varchar(32), last_name varchar(32), PRIMARY KEY (id))")
+        cur.execute("CREATE TABLE book(id int, name varchar(100), year date, description text, PRIMARY KEY (id))")
+        cur.execute("CREATE TABLE page(id int, book_id int, content text, is_torn boolean, PRIMARY KEY (id))")
+        cur.execute("CREATE TABLE book_author(book_id int, author_id int)")
+        self.conn.commit()
 
+    def create_value(self):
+        cur = self.conn.cursor()
+        cur.execute("INSERT INTO book(id, name, year, description) VALUES(1, 'Tails', '1994', 'Сказки народов мира')")
+        self.conn.commit()
+
+    def select_all_book(self):
+        cur = self.conn.cursor()
+        book = cur.execute("SELECT * from book")
+        print(book.fetchone())
--- libraryAPI.py
+++ libraryAPI.py
@@ -3,12 +3,13 @@ import aiohttp_cors
 import logging
 import json
 import datetime
+from db import DB
 
 logging.basicConfig(level="DEBUG")
 
 
 async def getAllBooks(request):
-    res = []
+    db.select_all_book()
 
 
 
@@ -21,34 +22,39 @@ cors = aiohttp_cors.setup(app, defaults={
     )
 })
 
-# All GET
+# # All GET
 app.add_routes([web.get("/library/books/", getAllBooks)])
-app.add_routes([web.get("/library/books/{book_id}", getBook)])
-app.add_routes([web.get("/library/books/{book_id}/pages/", getBookAllPages)])
-app.add_routes([web.get("/library/books/{book_id}/pages/{page_id}", getBookSinglePage)])
-app.add_routes([web.get("/library/books/{book_id}/authors/", getBookAuthors)])
-app.add_routes([web.get("/library/authors", getAuthors)])
-app.add_routes([web.get("/library/authors/{author_id}/books/", getAuthorBooks)])
-
-# All POST
-app.add_routes([web.post("/library/books/", addBook)])
-app.add_routes([web.post("/library/authors/{author_id}", addAuthorBooks)])
-
-# All PUT
-app.add_routes([web.put("/library/books/{book_id}", updateBook)])
-
-# All PATCH
-app.add_routes([web.patch("/library/books/{book_id}/pages/{page_id}", updateBookSinglePage)])
-
-# All DELETE
-app.add_routes([web.delete("/library/books/{book_id}", deleteBook)])
-
-# All HEAD
-app.add_routes([web.get("/library/books/{book_id}", getBookHEAD)])
+# app.add_routes([web.get("/library/books/{book_id}", getBook)])
+# app.add_routes([web.get("/library/books/{book_id}/pages/", getBookAllPages)])
+# app.add_routes([web.get("/library/books/{book_id}/pages/{page_id}", getBookSinglePage)])
+# app.add_routes([web.get("/library/books/{book_id}/authors/", getBookAuthors)])
+# app.add_routes([web.get("/library/authors", getAuthors)])
+# app.add_routes([web.get("/library/authors/{author_id}/books/", getAuthorBooks)])
+#
+# # All POST
+# app.add_routes([web.post("/library/books/", addBook)])
+# app.add_routes([web.post("/library/authors/{author_id}", addAuthorBooks)])
+#
+# # All PUT
+# app.add_routes([web.put("/library/books/{book_id}", updateBook)])
+#
+# # All PATCH
+# app.add_routes([web.patch("/library/books/{book_id}/pages/{page_id}", updateBookSinglePage)])
+#
+# # All DELETE
+# app.add_routes([web.delete("/library/books/{book_id}", deleteBook)])
+#
+# # All HEAD
+# app.add_routes([web.get("/library/books/{book_id}", getBookHEAD)])
 
 
 # for route in list(app.router.routes()):
 #     cors.add(route)
 
 if __name__ == '__main__':
+    x = DB()
+    x.create_connection()
+    x.create_db()
+    x.create_value()
+    x.select_all_book()
     web.run_app(app)
